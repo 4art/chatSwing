b8aa879db25638b833965be6d01f6b7cdf547538
ErikaO-K <erika_o-k@hotmail.de>
2018-01-11 13:25:04 +0100

    Created package model with the class Health

---
+++ src/main/java/com/github/chat/model/Health.java
@@ -0,0 +1,14 @@
+package com.github.chat.model;
+
+public class Health {
+    private boolean health=true;
+
+    public void setHealth(boolean health) {
+        this.health = health;
+
+    }
+    public boolean getHealth(){
+        return true;
+    }
+
+}
