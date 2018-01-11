ff79cd580be36a3b875e0da2d6d7b3eb2a942f75
ErikaO-K <erika_o-k@hotmail.de>
2018-01-11 16:47:45 +0100

    Test to weather server. Success

--- src/main/java/com/github/chat/service/Backend.java
+++ src/main/java/com/github/chat/service/Backend.java
@@ -2,20 +2,26 @@ package com.github.chat.service;
 
 
 import com.github.chat.model.Health;
+import com.github.chat.model.Weather;
+import com.sun.javafx.binding.StringFormatter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class Backend {
     private RestTemplate restTemplate = new RestTemplate();
-    public static void main(String[] args) {
-        Backend backend = new Backend();
-        System.out.println(backend.checkHealth().isHealth());
-    }
+
     public Health checkHealth(){
         ResponseEntity<Health> healthResponseEntity = restTemplate.getForEntity(
           "https://d1fuzbayh0.execute-api.eu-central-1.amazonaws.com/prod", Health.class
         );
         Health health = healthResponseEntity.getBody();
         return health;
+    }
+    public Weather checkWeather() {
+        ResponseEntity <Weather> weatherResponseEntity = restTemplate.getForEntity(
+                "https://d1fuzbayh0.execute-api.eu-central-1.amazonaws.com/prod/weather", Weather.class
+        );
+        Weather weather = weatherResponseEntity.getBody();
+        return weather;
     }
 }
