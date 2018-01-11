eeba1595097b7fc198d2179f4f64b5ab45e9d838
ErikaO-K <erika_o-k@hotmail.de>
2018-01-11 13:25:46 +0100

    Created package service with the class Backend

---
+++ src/main/java/com/github/chat/service/Backend.java
@@ -0,0 +1,11 @@
+package com.github.chat.service;
+
+
+import org.springframework.web.client.RestTemplate;
+
+public class Backend {
+    private RestTemplate restTemplate = new RestTemplate();
+    public static void main(String[] args) {
+
+    }
+}
